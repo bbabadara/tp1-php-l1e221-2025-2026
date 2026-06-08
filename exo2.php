4f4b6e48455f569c0a4801a3bf1ff8595d1acdff
Badara <bbabadara@gmail.com>
2026-06-08 15:03:50 +0000

    resulution du l'exo 2

---
+++ exo2.php
@@ -0,0 +1,18 @@
+<?php
+
+$fruits = ["Pomme", "Banane", "Orange", "Mangue"];
+
+
+// Afficher le nombre de fruits.
+echo "Le nombre de fruit est: ".count($fruits);
+
+// Ajouter un fruit à la fin du tableau.
+$fruits[]="Ananas";
+array_push($fruits,"Poire");
+echo "<br>";echo "<br>";
+
+// Réafficher le nombre de fruits.
+echo "Le nombre de fruit apres ajout est: ".count($fruits);
+
+var_dump($fruits);
+
