bc37e284acd0cd83920bb7fa0f0cff51aef1fa07
Badara <bbabadara@gmail.com>
2026-06-08 15:17:43 +0000

    resulution du l'exo 3

---
+++ exo3.php
@@ -0,0 +1,24 @@
+<?php
+$notes = [12, 15, 18, 10, 14];
+
+
+// Calculer la somme des notes.
+$somme = 0;
+$max=$notes[0];
+foreach($notes as $note){
+    $somme=$somme + $note;
+    if ($note>$max) {
+        $max=$note;
+    }
+}
+
+// Calculer la moyenne.
+$moyenne = $somme/count($notes);
+
+// Afficher la note la plus élevée.
+echo "La somme est : $somme ";
+echo "<br>";echo "<br>";
+echo "La moyenne est : $moyenne ";
+
+echo "<br>";echo "<br>";
+echo "La plus grande note est : $max ";
