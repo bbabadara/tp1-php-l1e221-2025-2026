497d08e1f216ed0a8d2c0302cfaf63fb77c26a6f
Badara <bbabadara@gmail.com>
2026-06-08 15:55:21 +0000

    resulution du l'exo 4

---
+++ exo4.php
@@ -0,0 +1,30 @@
+<?php
+$couleurs = ["Rouge", "Vert", "Bleu", "Jaune"];
+
+// Vérifier si "Bleu" existe dans le tableau.
+// Vérifier si "Noir" existe dans le tableau.
+$blueExist=false;
+$noirExist=false;
+foreach ($couleurs as $couleur) {
+    if ($couleur=="Bleu") {
+        $blueExist=true;
+    }
+    if ($couleur=="Noir") {
+        $noirExist=true;
+    }
+}
+
+// Afficher un message approprié.
+if ($blueExist) {
+   echo "La couleur blue est dans le tableau";
+}else {
+    echo "La couleur blue n'est pas dans le tableau";
+}
+
+echo "<br>";echo "<br>";
+
+if ($noirExist) {
+   echo "La couleur noire est dans le tableau";
+}else {
+    echo "La couleur noire n'est pas dans le tableau";
+}
