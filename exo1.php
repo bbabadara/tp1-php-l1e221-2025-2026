34107c741bc6ce3109d717e7afd2d05074997915
Badara <bbabadara@gmail.com>
2026-06-08 14:51:21 +0000

    resulution du l'exo 1

--- exo1.php
+++ exo1.php
@@ -1,1 +1,32 @@
 <?php
+// Créez un tableau contenant les jours de la semaine :
+// $jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi"];
+
+
+$jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi"];
+
+// Afficher le premier jour.
+echo "Le premier jour est: ".$jours[0];
+echo "<br>";echo "<br>";
+
+// Afficher le dernier jour.
+$taille=count($jours);
+
+echo "Le dernier jour est: ".$jours[$taille-1];
+
+echo "<br>";echo "<br>";
+
+// Afficher tous les jours avec une boucle.
+
+echo "Les jours de la semaine (avec for) sont: ";
+for ($i=0; $i <count($jours) ; $i++) {
+    echo $jours[$i].", ";
+}
+
+echo "<br>";echo "<br>";
+
+echo "Les jours de la semaine (avec foreach) sont: ";
+
+foreach ($jours as  $jour) {
+    echo $jour." ,";
+}
